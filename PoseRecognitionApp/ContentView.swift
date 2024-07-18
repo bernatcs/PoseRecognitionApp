7c5424404f261cc4841e9cad3b287f280194caf7
bernatcs <bercusi@gmail.com>
2024-07-18 08:56:23 +0200

    Initial Commit

---
+++ PoseRecognitionApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PoseRecognitionApp
+//
+//  Created by Bernat CS - SBP on 18/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PoseRecognitionApp/PoseRecognitionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoseRecognitionAppApp.swift
+//  PoseRecognitionApp
+//
+//  Created by Bernat CS - SBP on 18/7/24.
+//
+
+import SwiftUI
+
+@main
+struct PoseRecognitionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
