8eac85bde21f7fe8e7625f5f81bef37b82a46e14
bernatcs <bercusi@gmail.com>
2024-07-19 20:15:04 +0200

    act

--- PoseRecognitionApp/Instr_Piano.swift
+++ PoseRecognitionApp/Instr_Piano.swift
@@ -12,17 +12,19 @@ struct Instr_Piano: View {
     var body: some View {
         NavigationView {
             GeometryReader { geometry in
-                ZStack {
-                    if ProcessInfo.processInfo.isiOSAppOnMac, let url = Bundle.main.url(forResource: "Instr_Piano_demo", withExtension: "mp4") {
+                ZStack (alignment: .top) {
+                    if ProcessInfo.processInfo.isiOSAppOnMac, let url = Bundle.main.url(forResource: "Piano Posture", withExtension: "mov") {
                         QuickPoseSimulatedCameraView(useFrontCamera: false, delegate: quickPose, video: url)
                     } else {
                         QuickPoseCameraSwitchView(useFrontCamera: $useFrontCamera, delegate: quickPose)
                     }
                     QuickPoseOverlayView(overlayImage: $overlayImage)
                 }
-                .frame(width: geometry.size.width, height: geometry.size.height)
-                .aspectRatio(contentMode: .fill)
-                .clipped()
+//                .frame(width: geometry.size.width)
+                .frame(width: geometry.safeAreaInsets.leading + geometry.size.width + geometry.safeAreaInsets.trailing)
+//                .aspectRatio(contentMode: .fill)
+//                .clipped()
+                .edgesIgnoringSafeArea(.all)
                 .overlay(alignment: .bottom){
                     Button(action: {
                         useFrontCamera.toggle()
@@ -36,124 +38,33 @@ struct Instr_Piano: View {
                     .padding(.bottom, 20)
                 }
                 .onAppear {
-                    let basicStyle = QuickPose.Style(
+                    
+//                    let modelConfigLite = QuickPose.ModelConfig(detailedFaceTracking: false, detailedHandTracking: false, modelComplexity: .light)
+//                    let modelConfigGood = QuickPose.ModelConfig(detailedFaceTracking: true, detailedHandTracking: true, modelComplexity: .good)
+                    let modelConfigHeavy = QuickPose.ModelConfig(detailedFaceTracking: true, detailedHandTracking: true, modelComplexity: .heavy)
+                    
+                    let basicStyleHip = QuickPose.Style(
                         relativeFontSize: 0.5,
                         relativeArcSize: 0.2,
                         relativeLineWidth: 0.5,
-                        conditionalColors: [QuickPose.Style.ConditionalColor(min: 85, max: 100, color: UIColor.green)]
+                        conditionalColors: [QuickPose.Style.ConditionalColor(min: 95, max: 115, color: UIColor.green)]
                     )
                     
-                    quickPose.start(features: [.rangeOfMotion(.hip(side: .right, clockwiseDirection: false), style: basicStyle)]) { status, outputImage, _, _, _ in
+                    quickPose.start(features: [.rangeOfMotion(.hip(side: .right, clockwiseDirection: false), style: basicStyleHip)], modelConfig: modelConfigHeavy) { status, outputImage, _, _, _ in
                         overlayImage = outputImage
+                    
+//                    quickPose.start(features: [.overlay(.upperBody)], modelConfig: modelConfigHeavy) { status, outputImage, _, _, _ in
+//                        overlayImage = outputImage
+                        
+//                    quickPose.start(features: [.], modelConfig: modelConfigHeavy) { status, outputImage, _, _, _ in
+//                        overlayImage = outputImage
                     }
                 }
                 .onDisappear {
                     quickPose.stop()
                 }
             }
-            .edgesIgnoringSafeArea(.all)
         }
         .navigationViewStyle(StackNavigationViewStyle())
     }
 }
-
-#Preview {
-    Instr_Piano()
-}
-
-
-
-//import SwiftUI
-//import QuickPoseCore
-//import QuickPoseSwiftUI
-//
-//struct Instr_Piano: View {
-//    private var quickPose = QuickPose(sdkKey: sdkKey.sdkKey)
-//    @State private var overlayImage: UIImage?
-//    @State private var useFrontCamera: Bool = true
-//
-//    var body: some View {
-//        NavigationView {
-//            GeometryReader { geometry in
-//                ZStack {
-//                    if ProcessInfo.processInfo.isiOSAppOnMac, let url = Bundle.main.url(forResource: "Instr_Piano_demo", withExtension: "mp4") {
-//                        QuickPoseSimulatedCameraView(useFrontCamera: false, delegate: quickPose, video: url)
-//                            .aspectRatio(contentMode: .fill)
-//                            .frame(width: geometry.size.width, height: geometry.size.height)
-//                            .clipped()
-//                            .background(Color.black)
-//                    } else {
-//                        QuickPoseCameraSwitchView(useFrontCamera: $useFrontCamera, delegate: quickPose)
-//                            .aspectRatio(contentMode: .fill)
-//                            .frame(width: geometry.size.width, height: geometry.size.height)
-//                            .clipped()
-//                            .background(Color.black)
-//                    }
-//                    
-//                    QuickPoseOverlayView(overlayImage: $overlayImage)
-//                    
-//                    VStack {
-//                        Spacer()
-//                        HStack {
-//                            if geometry.size.width > geometry.size.height {
-//                                Spacer() // Empuja el botón a la derecha en landscape
-//                                Button(action: {
-//                                    useFrontCamera.toggle()
-//                                }) {
-//                                    Image(systemName: "arrow.triangle.2.circlepath.camera")
-//                                        .font(.system(size: 30))
-//                                        .padding()
-//                                        .background(Color.white.opacity(0.8))
-//                                        .clipShape(Circle())
-//                                }
-//                                .padding(.bottom, 20)
-//                                .padding(.trailing, 20)
-//                            } else {
-//                                Spacer() // Centra el botón horizontalmente en portrait
-//                                Button(action: {
-//                                    useFrontCamera.toggle()
-//                                }) {
-//                                    Image(systemName: "arrow.triangle.2.circlepath.camera")
-//                                        .font(.system(size: 30))
-//                                        .padding()
-//                                        .background(Color.white.opacity(0.8))
-//                                        .clipShape(Circle())
-//                                }
-//                                .padding(.bottom, 20)
-//                                Spacer() // Mantiene el botón centrado
-//                            }
-//                        }
-//                        .frame(width: geometry.size.width) // Asegura que HStack ocupe todo el ancho
-//                    }
-//                }
-//                .navigationBarHidden(true)
-//                .onAppear {
-//                    let basicStyle = QuickPose.Style(
-//                        relativeFontSize: 0.5,
-//                        relativeArcSize: 0.2,
-//                        relativeLineWidth: 0.5,
-//                        conditionalColors: [QuickPose.Style.ConditionalColor(min: nil, max: 99, color: UIColor.green)]
-//                    )
-//                    
-//                    quickPose.start(features: [.rangeOfMotion(.hip(side: .right, clockwiseDirection: false), style: basicStyle)]) { status, outputImage, _, _, _ in
-//                        if case .success = status {
-//                            overlayImage = outputImage
-//                        } else {
-//                            print("Error processing overlay image")
-//                        }
-//                    }
-//                }
-//                .onDisappear {
-//                    quickPose.stop()
-//                }
-//            }
-//            .edgesIgnoringSafeArea(.all)
-//            .aspectRatio(contentMode: .fill)
-//        }
-//        .navigationViewStyle(StackNavigationViewStyle())
-//    }
-//}
-//
-//#Preview {
-//    Instr_Piano()
-//}
